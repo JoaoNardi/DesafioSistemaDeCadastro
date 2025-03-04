3ec4553ca65e3a1de361448badef73ead12f5e93
João Nardi <joaovitornard@gmail.com>
2025-03-03 20:09:32 -0400

    Formata uma resposta vindo do input (altura).

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -10,10 +11,6 @@ public class Usuario {
     protected String id;
     protected List<String> listaDeRespostas = new ArrayList<>(); // lista para armazenar respostas
     protected File respostasTxt;
-
-    public String getNome() {
-        return nome;
-    }
 
     ArquivoPerguntas pergunta = new ArquivoPerguntas(); //inicializa a classe que converteu o txt para uma lista string
     Scanner leitorResposta = new Scanner(System.in); // inicializa a classe scanner para capturar input
@@ -33,29 +30,34 @@ public class Usuario {
                 }
             }
             this.numero = new PastaUsuarios().contaUsuarios() + 1; // gera um numero para o usuario com base na quantidade de usuarios que existem
-            this.nome = listaDeRespostas.get(0);
+            this.nome = listaDeRespostas.getFirst();
             this.id = numero + "-" + nome.replace(" ", "").toUpperCase(); // gera o id (nome do txt do usuario)
 
+            DecimalFormat formatoMetros = new DecimalFormat("0,00"); //define um formato para metros
 
+        // separa o valor numerico do restante e substitui "," por "."
+            String[] partes = listaDeRespostas.get(3).replace(",","").replace(".","").split(" ");
+            double alturaConvertida = Double.parseDouble(partes[0]); // converte a altura em double
+            String alturaAjustada = formatoMetros.format(alturaConvertida); //transforma a altura em string no formato desejado
+            listaDeRespostas.set(3,alturaAjustada); // subtitui a altura antiga por uma altura formatada nova
     }
     public void criaUsuario() throws IOException { // metodo para criar usuario
         try {
             capturaInput();
-            if (listaDeRespostas.get(0).length() < 10){
+            if (listaDeRespostas.get(0).length() < 10){ // se o nome tiver menos de 10 caracteres
                 throw new InputMismatchException("Erro: NOME COMPLETO inválido! (deve conter ao menos 10 caracteres)");
             }else {
-                String email = listaDeRespostas.get(1).trim();
-                System.out.println("email" + email);
+                String email = listaDeRespostas.get(1).trim(); // se o email nao tiver o @
                 if (!email.contains("@")){
                     throw new InputMismatchException("Erro: EMAIL inválido! (deve conter @)");
                 } else {
-                    if ((Integer.parseInt(listaDeRespostas.get(2)) <18)){
+                    if ((Integer.parseInt(listaDeRespostas.get(2)) <18)){ // se o input de idade for menor que 18 anos
                         throw new IllegalArgumentException("Erro: Usuario deve ter mais de 18 anos");
-
                     }
+
                 }
                 respostasTxt = new File("C:/Users/João V Nardi/Desktop/Joao/java/DesafioSistemaDeCadastro2/usuarios", id + ".txt");
-                respostasTxt.createNewFile(); // cria o txt com base no diretorio acima
+//                respostasTxt.createNewFile(); // cria o txt com base no diretorio acima
                 //bloco para escrever no txt
                 OutputStream os = new FileOutputStream(respostasTxt);
                 Writer wr = new OutputStreamWriter(os);
@@ -69,8 +71,4 @@ public class Usuario {
 
         }
     }
-    public File getRespostasTxt() {
-        return respostasTxt;
-    }
-
 }
